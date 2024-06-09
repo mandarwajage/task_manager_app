5011e31f28aa57275ca646d17709edcb2f524922
karishmapatil18 <karishmapatil185@gmail.com>
2024-06-09 22:57:05 +0530

    Added Component

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -32,7 +32,7 @@ const Login = () => {
               Manage all your Task in one place
             </span>
             <p className='flex flex-col gap-0 md:gap-4 text-4xl md:text-6xl 2xl:text-7xl font-black text-center text-blue-700'>
-              <span>Clouse-Based</span>
+              <span>Cloud-Based</span>
               <span>Task Manager</span>
             </p>
             <div className='cell'>
