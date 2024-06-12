eac5f892c0aa6c4ad7add083fb9c5cb12e9fd95a
mandar wajage <mandarwajage@gmail.com>
2024-06-12 21:42:14 +0530

    added authSlice.js

--- client/src/redux/slices/authSlice.js
+++ client/src/redux/slices/authSlice.js
@@ -1,27 +1,31 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const intitalState = {
-  user: localStorage.getItem('userInfo') 
-    ? JSON.parse(localStorage.getItem('userInfo'))
+const initialState = {
+  user: localStorage.getItem("userInfo")
+    ? JSON.parse(localStorage.getItem("userInfo"))
     : null,
-  
+
   isSidebarOpen: false,
 };
 
 const authSlice = createSlice({
-  name:"auth",
+  name: "auth",
   initialState,
-  reducers:{
-    setCredentials : (state,actions)=> {
+  reducers: {
+    setCredentials: (state, action) => {
       state.user = action.payload;
-      localStorage.setItem("userInfo", JSON.stringify(action.payload))
+      localStorage.setItem("userInfo", JSON.stringify(action.payload));
     },
-    logout:(state, action) => {
+    logout: (state, action) => {
       state.user = null;
       localStorage.removeItem("userInfo");
     },
-    setOpenSidenar:(state,action) => {
-      state.insSidebaropen = action.payload;
-    }
-  }
-})
+    setOpenSidebar: (state, action) => {
+      state.isSidebarOpen = action.payload;
+    },
+  },
+});
+
+export const { setCredentials, logout, setOpenSidebar } = authSlice.actions;
+
+export default authSlice.reducer;
