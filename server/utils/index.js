dd4fd1e1120d93112a9b910294c88f462a761d87
mandar wajage <mandarwajage@gmail.com>
2024-06-20 18:31:07 +0530

    added backend code

---
+++ server/utils/index.js
@@ -0,0 +1,36 @@
+import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+
+export const dbConnection = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+
+    console.log("DB connection established");
+  } catch (error) {
+    console.log("DB Error: " + error);
+  }
+};
+
+export const createJWT = (res, userId) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
+
+  // Log the token for debugging
+  console.log('JWT Token:', token);
+
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== "development",
+    sameSite: "strict", // Prevent CSRF attack
+    maxAge: 1 * 24 * 60 * 60 * 1000, // 1 day
+  });
+
+  // Log the cookie settings for debugging
+  console.log('Cookie settings:', {
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== "development",
+    sameSite: "strict",
+    maxAge: 1 * 24 * 60 * 60 * 1000,
+  });
+};
