2e681609a69f175984bf7b7ab65bd7061594c79b
mandar wajage <mandarwajage@gmail.com>
2024-06-21 02:29:16 +0530

    Completed Logout feature

--- client/src/components/UserAvatar.jsx
+++ client/src/components/UserAvatar.jsx
@@ -5,6 +5,9 @@ import { IoLogOutOutline } from "react-icons/io5";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { getInitials } from "../utils";
+import { toast } from "sonner";
+import { useLogoutMutation } from "../redux/slices/api/authApiSlice";
+import { logout } from "../redux/slices/authSlice";
 
 const UserAvatar = () => {
   const [open, setOpen] = useState(false);
@@ -13,41 +16,51 @@ const UserAvatar = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const logoutHandler = () => {
+  const [logoutUser] = useLogoutMutation();
+
+  const logoutHandler = async () => {
     console.log("logout");
+
+    try {
+      await logoutUser().unwrap();
+      dispatch(logout());
+      navigate("/log-in");
+    } catch (error) {
+      toast.error("Something went wrong", error);
+    }
   };
 
   return (
     <>
       <div>
-        <Menu as='div' className='relative inline-block text-left'>
+        <Menu as="div" className="relative inline-block text-left">
           <div>
-            <Menu.Button className='w-10 h-10 2xl:w-12 2xl:h-12 items-center justify-center rounded-full bg-blue-600'>
-              <span className='text-white font-semibold'>
-                {getInitials("Mandar Wajage")}   
-               {/* {getInitials(user?.name)} ##TODO */}
+            <Menu.Button className="w-10 h-10 2xl:w-12 2xl:h-12 items-center justify-center rounded-full bg-blue-600">
+              <span className="text-white font-semibold">
+                {getInitials("Mandar Wajage")}
+                {/* {getInitials(user?.name)} ##TODO */}
               </span>
             </Menu.Button>
           </div>
 
           <Transition
             as={Fragment}
-            enter='transition ease-out duration-100'
-            enterFrom='transform opacity-0 scale-95'
-            enterTo='transform opacity-100 scale-100'
-            leave='transition ease-in duration-75'
-            leaveFrom='transform opacity-100 scale-100'
-            leaveTo='transform opacity-0 scale-95'
+            enter="transition ease-out duration-100"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
           >
-            <Menu.Items className='absolute right-0 mt-2 w-56 origin-top-right divide-gray-100 rounded-md bg-white shadow-2xl ring-1 ring-black/5 focus:outline-none'>
-              <div className='p-4'>
+            <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-gray-100 rounded-md bg-white shadow-2xl ring-1 ring-black/5 focus:outline-none">
+              <div className="p-4">
                 <Menu.Item>
                   {({ active }) => (
                     <button
                       onClick={() => setOpen(true)}
-                      className='text-gray-700 group flex w-full items-center rounded-md px-2 py-2 text-base'
+                      className="text-gray-700 group flex w-full items-center rounded-md px-2 py-2 text-base"
                     >
-                      <FaUser className='mr-2' aria-hidden='true' />
+                      <FaUser className="mr-2" aria-hidden="true" />
                       Profile
                     </button>
                   )}
@@ -59,7 +72,7 @@ const UserAvatar = () => {
                       onClick={() => setOpenPassword(true)}
                       className={`tetx-gray-700 group flex w-full items-center rounded-md px-2 py-2 text-base`}
                     >
-                      <FaUserLock className='mr-2' aria-hidden='true' />
+                      <FaUserLock className="mr-2" aria-hidden="true" />
                       Change Password
                     </button>
                   )}
@@ -71,7 +84,7 @@ const UserAvatar = () => {
                       onClick={logoutHandler}
                       className={`text-red-600 group flex w-full items-center rounded-md px-2 py-2 text-base`}
                     >
-                      <IoLogOutOutline className='mr-2' aria-hidden='true' />
+                      <IoLogOutOutline className="mr-2" aria-hidden="true" />
                       Logout
                     </button>
                   )}
